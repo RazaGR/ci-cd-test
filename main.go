b9bff7575fb2eb407e498e88a9753f5ae739a156
Mohsin Raza <mohsin.raza@pensionera.se>
2023-12-01 08:32:56 +0100

    fix: platform update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -11,5 +12,23 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	http.HandleFunc("/", hello)
+
+	// Liveness Probe
+	go func() {
+		http.HandleFunc("/alive", func(w http.ResponseWriter, req *http.Request) {
+			fmt.Fprintf(w, "alive\n")
+		})
+		http.ListenAndServe(":8091", nil)
+	}()
+
+	// Readiness Probe
+	go func() {
+		time.Sleep(10 * time.Second) // Delay the readiness probe for 10 seconds
+		http.HandleFunc("/ready", func(w http.ResponseWriter, req *http.Request) {
+			fmt.Fprintf(w, "ready\n")
+		})
+		http.ListenAndServe(":8092", nil)
+	}()
+	fmt.Println("Server started")
 	http.ListenAndServe(":8090", nil)
 }
